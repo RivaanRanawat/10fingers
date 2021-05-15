1245eda1b6912f4348c360be3b363e0406cb6d7c
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-05-15 16:36:09 +0530

    setting up the files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const socketio = require("socket.io");
+
+const app = express()
+const server = app.listen(3001);
+const io = socketio(server);
+
+mongoose.connect("mongodb://localhost:27017/typeRacingClone", {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log("connected to database")
+})
